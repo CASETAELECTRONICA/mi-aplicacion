044b6a2fd65f0da68f310e9d10aabddb6896c58c
CASETAELECTRONICA <casetaelectronica@gmail.com>
2025-04-01 15:00:16 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ def mostrar_registros():
     # Conexión a la base de datos
     conn = sqlite3.connect('equipos.db')
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM Datos")  # Asegúrate de que 'Datos' sea el nombre de tu tabla
+    cursor.execute("SELECT * FROM Datos")  # Verifica que 'Datos' sea el nombre correcto de la tabla
     registros = cursor.fetchall()
     conn.close()
     # Renderiza la plantilla registros.html
