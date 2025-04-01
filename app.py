6eac22747cfa4a8f980c41a03a63c6b1befc71d7
CASETAELECTRONICA <casetaelectronica@gmail.com>
2025-04-01 14:47:56 -0600

    Update app.py

--- app.py
+++ app.py
@@ -78,3 +78,15 @@ def ranking():
 if __name__ == '__main__':
     crear_tabla()
     app.run(debug=True)
+
+
+@app.route('/registros')
+def mostrar_registros():
+    # Conexión a la base de datos
+    conn = sqlite3.connect('equipos.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM Datos")  # Asegúrate de que 'Datos' sea el nombre de tu tabla
+    registros = cursor.fetchall()
+    conn.close()
+    # Renderiza la plantilla registros.html
+    return render_template('registros.html', registros=registros)
